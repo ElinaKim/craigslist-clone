63de3c558a3592b4d91dfa004647a7c47012fe4c
Elina Kim <elinakim791@gmail.com>
2023-03-25 00:44:07 -0700

    created user model

--- src/app.py
+++ src/app.py
@@ -10,5 +10,17 @@ db = SQLAlchemy(app)
 def index():
     return "Hello World"
 
+class User(db.Model):
+    __tablename__ = 'my_users'
+    id = db.Column(db.Integer(), primary_key = True)
+    username = db.Column(db.String(65), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username):
+        self.username = username
+    
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+    
 if __name__ == '__main__':
     app.run()
