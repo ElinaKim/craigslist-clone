d5c36480150ccae01f9f44de2fc63b031e358fdc
Elina Kim <elinakim791@gmail.com>
2023-03-26 14:55:25 -0700

    create sub_category model

--- src/app.py
+++ src/app.py
@@ -28,5 +28,11 @@ class Category(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
     name = db.Column(db.String(65), unique=True, nullable=False)
 
+class SubCategory(db.Model):
+    __tablename__ = 'sub_category'
+    id = db.Column(db.Integer(), primary_key = True)
+    name = db.Column(db.String(65), unique=True, nullable=False)
+    category_id = db.Column(db.Integer(), db.ForeignKey('category.id'))
+
 if __name__ == '__main__':
     app.run()
