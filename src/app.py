a7479f26584c9f581ef45ffba21f9119cddfbffd
Elina Kim <elinakim791@gmail.com>
2023-04-04 20:39:05 -0700

    create listing and listing_category table

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -15,6 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
     username = db.Column(db.String(65), unique=True)
     email = db.Column(db.String(120), unique=True)
+    listings = db.relationship('Listing', backref='listing', lazy='dynamic')
 
     def __init__(self, username, email):
         self.username = username
@@ -22,17 +24,30 @@ class User(db.Model):
     
     def __repr__(self):
         return "<User '{}'>".format(self.username)
-
+    
 class Category(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer(), primary_key = True)
     name = db.Column(db.String(70), unique=True, nullable=False)
-    parent_category_id = db.Column(db.Integer(), db.ForeignKey('category.id'), nullable=False)
+    parent_category_id = db.Column(db.Integer(), db.ForeignKey('category.id'))
 
     sub_categories = db.relationship('Category', remote_side=[id], backref='parent_category')
 
     def __repr__(self):
         return f"<Category(id={self.id}, name='{self.name}')>"
+
+listing_category = db.Table('listing_category', db.Column('listing_id', db.Integer(), db.ForeignKey('listing.id')),
+db.Column('category_id', db.Integer(), db.ForeignKey('category.id')))
+
+class Listing(db.Model):
+    __tablename__ = 'listing'
+    id = db.Column(db.Integer(), primary_key = True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    date_update = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('my_users.id'))
+    categories = db.relationship('Category', secondary='listing_category', backref=db.backref('listings', lazy='dynamic'))
 
 
 if __name__ == '__main__':
